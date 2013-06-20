b50f168f7c608cb48e90a69c530a7f91784fa0b8
Rodrigo Campos <rodrigocampos@udec.cl>
2013-06-20 10:42:57 -0400

    creado el grud para Place

---
+++ src/Jooks/WebServiceBundle/Form/PlaceType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Jooks\WebServiceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Jooks\WebServiceBundle\Entity\Place'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'jooks_webservicebundle_placetype';
+    }
+}
